8f03a5caa05673877c867a64bd2ac494e7a5d612
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2024-07-12 20:25:54 -0300

    hostingAccountController

---
+++ app/Http/Controllers/HostingAccountController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HostingAccount;
+use Illuminate\Http\Request;
+
+class HostingAccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    /**
+     * Display a listing of the resource for client id.
+     */
+
+    public function showByClient(string $clientId)
+    {
+        $hostsAccount = HostingAccount::where('client_id', $clientId)->with('plan')->get();
+
+        return $this->success($hostsAccount);
+    }
+}
